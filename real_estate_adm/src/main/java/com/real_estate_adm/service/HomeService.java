ead4ad74c842928d548cc5bc7582bd973b8c9eb5
kht_dq <rlagudxo789@naver.com>
2018-08-23 18:05:04 +0900

    20180823 select박스 변경 작업 남음

---
+++ real_estate_adm/src/main/java/com/real_estate_adm/service/HomeService.java
@@ -0,0 +1,10 @@
+package com.real_estate_adm.service;
+
+import java.util.List;
+
+import com.real_estate_adm.vo.RegionVO;
+
+public interface HomeService {
+	public List<RegionVO> selDo() throws Exception;
+	public List<RegionVO> selSi(int region_do_val) throws Exception;
+}
---
+++ real_estate_adm/src/main/java/com/real_estate_adm/service/HomeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.real_estate_adm.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Repository;
+
+import com.real_estate_adm.dao.HomeDao;
+import com.real_estate_adm.vo.RegionVO;
+import com.sun.istack.internal.logging.Logger;
+
+@Repository
+public class HomeServiceImpl implements HomeService {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Inject
+	private HomeDao homeDao;
+	
+	public List<RegionVO> selDo() throws Exception {
+		List<RegionVO> list = homeDao.selDo();
+		return list;
+	}
+
+	public List<RegionVO> selSi(int region_do_val) throws Exception {
+		List<RegionVO> list = homeDao.selSi(region_do_val);
+		return list;
+	}
+}
